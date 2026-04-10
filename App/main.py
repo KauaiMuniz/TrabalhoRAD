fe746b6db9e9ba054fc66f696b0c684213ad0405
kauaimuniz <kauaimuniztomaz23@gmail.com>
2026-04-10 20:06:04 -0300

    ajustes

--- App/main.py
+++ App/main.py
@@ -3,6 +3,8 @@ from App import models
 from App.database import engine
 from App.routers import cliente, fornecedor, produto, venda, compra
 
+import uvicorn
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Loja de Roupas - API CRUD")
@@ -12,3 +14,6 @@ app.include_router(fornecedor.router)
 app.include_router(produto.router)
 app.include_router(venda.router)
 app.include_router(compra.router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
